10343ff830b04225b77f77aef0d3e1a83c33416e
Gabriel Tijon da Silva <75736869+TijonSilva@users.noreply.github.com>
2022-02-23 22:10:29 -0300

    criando banco do projeto

---
+++ estrtura ramal hbr.sql
@@ -0,0 +1,86 @@
+create database ramalhbr
+default character set utf8
+default collate utf8_general_ci;
+
+
+use ramalhbr;
+
+/*AIR BIP*/
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
+create table airbip (
+id int not null auto_increment,
+nome varchar (30) not null,
+ramal char (9) not null,
+primary key (id)
+)default charset = utf8 ;
+
